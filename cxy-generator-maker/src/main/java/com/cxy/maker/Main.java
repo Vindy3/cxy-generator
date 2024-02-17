3a48cd359622d6ed9ade48b55d2f53842783e12d
cxy <vindycxy@163.com>
2024-02-18 05:37:03 +0800

    代码优化

--- cxy-generator-maker/src/main/java/com/cxy/maker/Main.java
+++ cxy-generator-maker/src/main/java/com/cxy/maker/Main.java
@@ -1,15 +1,15 @@
 package com.cxy.maker;
 
-import com.cxy.maker.cli.CommandExecutor;
+import com.cxy.maker.generator.main.MainGenerator;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-       // args = new String[]{"generate", "-l", "-a", "-o"};
-//        args = new String[]{"config"};
-//        args = new String[]{"list"};
-        CommandExecutor commandExecutor = new CommandExecutor();
-        commandExecutor.doExecute(args);
+    public static void main(String[] args) throws TemplateException, IOException, InterruptedException {
+        MainGenerator mainGenerator = new MainGenerator();
+        mainGenerator.doGenerate();
     }
 }
